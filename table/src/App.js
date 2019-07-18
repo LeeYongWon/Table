8bbb263a9cfe86d5b8f3a739fe28417813cd9ef1
LeeYongwon <dyddnjs0982@naver.cpm>
2019-07-18 20:41:04 +0900

    filter로 삭제 선택한 값의 index와  배열의 남은 값들의 index를 비교해서

--- table/src/App.js
+++ table/src/App.js
@@ -3,8 +3,8 @@ import Table from './Table';
 import './App.css';
 
 class App extends Component {
-  render() {
-    const characters = [
+  state = {
+    characters: [
       {
         name: 'Charlie',
         job: 'Janitor'
@@ -15,14 +15,30 @@ class App extends Component {
       }, {
         name: 'Dee',
         job: 'Aspring actress'
-      }, {
+      },
+      {
         name: 'Dennis',
         job: 'Bartender'
-      },
+      }
     ]
+  }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+    this.setState({
+      characters:characters.filter((character, i)=>{
+        console.log("removeCharacter에서의 i "+i);
+        console.log("removeCharacter에서의 index "+index);
+        return i !== index
+      })
+    })
+  }
+
+  render() {
+    const { characters } = this.state
     return (
       <div className="container">
-        <Table characterData={characters}/>
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
       </div>
     );
   }
--- table/src/Table.js
+++ table/src/Table.js
@@ -4,23 +4,26 @@ import React, { Component } from 'react';
 
 class Table extends Component {
     render() {
-        const {characterData} =this.props
+        const {characterData, removeCharacter} =this.props
         return (
             <table>
                 <TableHeader />
-                <TableBody characterData={characterData} />
+                <TableBody characterData={characterData} removeCharacter={removeCharacter} />
             </table>
         );
     }
 }
 
 const TableBody = (table) => {
-
     const rows=table.characterData.map((value, index)=>{
+        console.log(`TableBody에서 ${index}`);
         return (
             <tr key={index+1}>
                 <td>{value.name}</td>
                 <td>{value.job}</td>
+                <td>
+                    <button onClick={() => table.removeCharacter(index)}>삭제버틍 </button>
+                </td>
             </tr>
         );
     })
